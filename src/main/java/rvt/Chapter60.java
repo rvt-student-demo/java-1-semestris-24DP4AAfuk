a4736fcdde84990084e3c7dd2445116a288fa736
Aleksejs Afuksenidi <A242366AA@rvt.lv>
2025-10-17 11:56:18 +0300

    Chapter60 - finished

--- src/main/java/rvt/Chapter60.java
+++ src/main/java/rvt/Chapter60.java
@@ -5,32 +5,32 @@ public class Chapter60 {
         // ex1();
         // ex2();
         // ex3();
-        ex4();
+        // ex4();
         // ex5();
     }
 
     public static void ex1() {
-        int[] val1 = {0, 1 , 2, 3};
+        int[] val = {0, 1 , 2, 3};
         int sum = 0;
 
-        for (int i = 0; i < val1.length; i++) {
-            sum += val1[i];
+        for (int i = 0; i < val.length; i++) {
+            sum += val[i];
         }
         System.out.println("Sum is: " + sum);
     }
     public static void ex2() {
-        int[] val2 = {13, -4, 82, 17};
-        int[] twice = new int[val2.length];
+        int[] val = {13, -4, 82, 17};
+        int[] twice = new int[val.length];
 
         System.out.print("Original array: ");
-        for (int i = 0; i < val2.length; i++) {
-            System.out.print(val2[i] + " ");
+        for (int i = 0; i < val.length; i++) {
+            System.out.print(val[i] + " ");
         }
 
         System.out.println();
 
-        for (int i = 0; i < val2.length; i++) {
-            twice[i] = val2[i] * 2;
+        for (int i = 0; i < val.length; i++) {
+            twice[i] = val[i] * 2;
         }
 
         System.out.print("\nNew array: ");
@@ -53,9 +53,41 @@ public class Chapter60 {
         }
     }
     public static void ex4() {
-        // code here
-    }
+        int[] valA = {13, -22, 82, 17};
+        int[] valB = {0, 0, 0, 0};
+
+        for (int i = 0; i < valA.length; i++) {
+            valB[i] = 25 - valA[i];
+        }
+
+        System.out.println( "valA: " 
+        + valA[0] + " " + valA[1] + " " + valA[2] + " " + valA[3] );
+ 
+        System.out.println( "valB: " 
+        + valB[0] + " " + valB[1] + " " + valB[2] + " " + valB[3] );
+
+        System.out.println( "sum:  " 
+        + (valA[0]+valB[0]) + " " + (valA[1]+valB[1]) + " " 
+        + (valA[2]+valB[2]) + " " + (valA[3]+valB[3]) );
+   }
     public static void ex5() {
-        // code here
+        int[] val = {0, 1, 2, 3};
+        int temp;
+
+        System.out.print("Original array: ");
+        for (int i = 0; i < val.length; i++) {
+            System.out.print(val[i] + " ");
+        }
+
+        for (int i = 0; i < val.length / 2; i++) {
+            temp = val[i];
+            val[i] = val[val.length - 1 - i];
+            val[val.length - 1 - i] = temp;
+        }
+
+        System.out.print("\nReversed array: ");
+        for (int i = 0; i < val.length; i++) {
+            System.out.print(val[i] + " ");
+        }
     }
 }
