5eaf1a9d079963531f8436138e3138faaecb07dc
Aleksejs Afuksenidi <A242366AA@rvt.lv>
2025-12-09 09:26:31 +0200

    Person and Subclasses - Part 2

---
+++ src/main/java/rvt/Student.java
@@ -0,0 +1,22 @@
+package rvt;
+
+public class Student extends Person {
+    private int credits;
+
+    public Student(String name, String address) {
+        super(name, address);
+        this.credits = 0;
+    }
+
+    public void study() {
+        this.credits++;
+    }
+
+    public static void main(String[] args) {
+        Student student = new Student("John Doe", "123 Main St");
+        System.out.println(student);
+        student.study();
+        student.study();
+        System.out.println("Study credits " + student.credits);
+    }
+}
