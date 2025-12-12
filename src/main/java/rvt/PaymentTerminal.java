7cd24f3bb0d028ad704240ab2c2c2c5795cf7cd0
Aleksejs Afuksenidi <A242366AA@rvt.lv>
2025-12-12 09:21:51 +0200

    Card payments - Part 2

---
+++ src/main/java/rvt/PaymentTerminal.java
@@ -0,0 +1,36 @@
+package rvt;
+
+public class PaymentTerminal {
+    private double money;  // amount of cash
+    private int affordableMeals; // number of sold affordable meals
+    private int heartyMeals;  // number of sold hearty meals
+
+    public PaymentTerminal() {
+        this.money = 1000;
+        this.affordableMeals = 0;
+        this.heartyMeals = 0;
+    }
+
+    public double eatAffordably(double payment) {
+        if (payment >= 2.50) {
+            this.money += 2.50;
+            this.affordableMeals++;
+            return payment - 2.50;
+        }
+        return payment;
+    }
+
+    public double eatHeartily(double payment) {
+        if (payment >= 4.30) {
+            this.money += 4.30;
+            this.heartyMeals++;
+            return payment - 4.30;
+        }
+        return payment;
+    }
+
+    public String toString() {
+        return "money: " + money + ", number of sold afforable meals: " + affordableMeals + ", number of sold hearty meals: " + heartyMeals;
+    }
+}
+
