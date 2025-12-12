6b45b2b9cad0930aeba5fd56d852c1e639b0dcad
Aleksejs Afuksenidi <A242366AA@rvt.lv>
2025-12-12 09:39:39 +0200

    Card payments - Part 4

--- src/main/java/rvt/PaymentTerminal.java
+++ src/main/java/rvt/PaymentTerminal.java
@@ -45,6 +45,13 @@ public class PaymentTerminal {
         return false;
     }
 
+    public void addMoneyToCard(PaymentCard card, double sum) {
+        if (sum > 0) {
+            card.addMoney(sum);
+            this.money += sum;
+        }
+    }
+
     public String toString() {
         return "money: " + money + ", number of sold afforable meals: " + affordableMeals + ", number of sold hearty meals: " + heartyMeals;
     }
